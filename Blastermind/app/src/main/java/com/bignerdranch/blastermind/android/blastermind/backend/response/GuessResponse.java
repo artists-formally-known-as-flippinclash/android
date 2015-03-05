442548f7d83f086630cade957ff785401c87a496
Jason Atwood <jatwood@bignerdranch.com>
2015-03-05 12:49:15 -0500

    builds guess response

--- Blastermind/app/src/main/java/com/bignerdranch/blastermind/android/blastermind/backend/response/GuessResponse.java
+++ Blastermind/app/src/main/java/com/bignerdranch/blastermind/android/blastermind/backend/response/GuessResponse.java
@@ -1,4 +1,26 @@
 package com.bignerdranch.blastermind.android.blastermind.backend.response;
 
+import com.google.gson.annotations.SerializedName;
+
 public class GuessResponse {
+    @SerializedName("data")
+    Data data;
+
+    private class Data {
+        @SerializedName("type")
+        String dataTtype;
+        @SerializedName("id")
+        int id;
+        @SerializedName("outcome")
+        String outcome;
+        @SerializedName("feedback")
+        Feedback feedback;
+
+        private class Feedback {
+            @SerializedName("color-count")
+            int colorCount;
+            @SerializedName("position-count")
+            int positionCount;
+        }
+    }
 }
