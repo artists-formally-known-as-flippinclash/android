adc2f774b2d47c05695c6004c02f35154cf1d38b
Jason Atwood <jatwood@bignerdranch.com>
2015-04-05 01:01:25 -0400

    cleanup model layer with Lombok

--- Blastermind/core/src/main/java/com/bignerdranch/blastermind/andorid/core/Guess.java
+++ Blastermind/core/src/main/java/com/bignerdranch/blastermind/andorid/core/Guess.java
@@ -2,18 +2,19 @@ package com.bignerdranch.blastermind.andorid.core;
 
 import java.util.List;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(prefix = "m")
 public class Guess {
 
-    private List<Logic.TYPE> mTypes;
+    private final List<Logic.TYPE> mTypes;
 
     public Guess(List<Logic.TYPE> types) {
         if (types.size() != Logic.guessWidth) {
             throw new IllegalArgumentException("types must be of size: " + Logic.guessWidth);
         }
         this.mTypes = types;
-    }
-
-    public List<Logic.TYPE> getTypes() {
-        return mTypes;
     }
 }
--- Blastermind/core/src/main/java/com/bignerdranch/blastermind/andorid/core/MatchEnd.java
+++ Blastermind/core/src/main/java/com/bignerdranch/blastermind/andorid/core/MatchEnd.java
@@ -1,41 +1,13 @@
 package com.bignerdranch.blastermind.andorid.core;
 
-public class MatchEnd {
+import lombok.Data;
+import lombok.experimental.Accessors;
 
+@Data
+@Accessors(prefix = "m")
+public class MatchEnd {
     private int mMatchId;
     private int mWinnerId;
     private String mWinnerName;
-    private Guess solution;
-
-    public int getMatchId() {
-        return mMatchId;
-    }
-
-    public void setMatchId(int matchId) {
-        mMatchId = matchId;
-    }
-
-    public int getWinnerId() {
-        return mWinnerId;
-    }
-
-    public void setWinnerId(int winnerId) {
-        mWinnerId = winnerId;
-    }
-
-    public String getWinnerName() {
-        return mWinnerName;
-    }
-
-    public void setWinnerName(String winnerName) {
-        mWinnerName = winnerName;
-    }
-
-    public Guess getSolution() {
-        return solution;
-    }
-
-    public void setSolution(Guess solution) {
-        this.solution = solution;
-    }
+    private Guess mSolution;
 }
